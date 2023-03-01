1948a9999072cfbff6973f6bd8368cf6a191b825
Akash Dutta <adwork1998@gmail.com>
2023-03-01 21:36:32 +0530

    Created Paddle A

--- Pong.py
+++ Pong.py
@@ -4,8 +4,21 @@ import turtle
 wn = turtle.Screen()
 wn.title("Pong by @itz_akashdutta")
 wn.bgcolor("black")
-wn.setup(height=800, width=600)
+wn.setup(height=600, width=800)
 wn.tracer(0) #set the screen update to 0
+
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) # Set the animation speed of paddle A to max speed ---> It will otherwise be very slow
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+
+# Ball
 
 
 # Main Game Loop
